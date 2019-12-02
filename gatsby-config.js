48954d2ac27163af237bc6b3e4c565b17863cf05
Daniel Doblado <danieldoblado@gmail.com>
2019-12-02 21:05:09 +0100

    Update images and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Daniel Doblado',
+        short_name: 'DobladoV',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'hsl(0, 0%, 95%)',
+        theme_color: 'hsl(180, 3%, 23%)',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/logo256.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-emotion',
