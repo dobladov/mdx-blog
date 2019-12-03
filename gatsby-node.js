001c29a6816c181a7cc00992df593c3ceb2c110d
Daniel Doblado <danieldoblado@gmail.com>
2019-12-03 08:25:53 +0100

    Add basic sitemap without styles

--- gatsby-node.js
+++ gatsby-node.js
@@ -97,3 +97,123 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
+
+exports.onPostBuild = async ({ graphql }) => {
+  const pathsQuery = await graphql(`
+    query  {
+      allSitePage {
+        edges {
+          node {
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  pathsQuery.data.allSitePage.edges.forEach(edge => {
+    console.log(edge.node.path)
+  })
+
+  const urls = pathsQuery.data.allSitePage.edges.filter(edge => !edge.node.path.includes('404')).map(edge => (
+    `
+    <url>
+    <loc>https://${edge.node.path}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+    </url>
+    `
+  ))
+
+  const sitemap = `
+    <?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls.join('\n')}
+    </urlset>
+  `
+
+  const sitemapStyle = `
+    <xsl:stylesheet version="2.0" 
+      xmlns:html="http://www.w3.org/TR/REC-html40"
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:sitemap="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    >
+      <xsl:output method="html" version="1.0" encoding="UTF-8" indent="yes"/>
+      <xsl:template match="/">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <title>Sitemap | </title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            
+            <style type="text/css">
+            </style>
+          </head>
+          <body>
+            <div class="wrapper">
+              <h1>
+                <a href="/">
+                  XML Sitemap
+                </a>
+              </h1>
+              <table id="sitemap" cellpadding="3">
+                <thead>
+                  <tr>
+                    <th width="75%">
+                      URL (<xsl:value-of select="count(sitemap:urlset/sitemap:url)"/> total)
+                    </th>
+                    <th width="5%">Priority</th>
+                    <th width="5%">Images</th>
+                    <th width="5%">Change Freq.</th>
+                    <th width="10%">Last Change</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <xsl:variable name="lower" select="'abcdefghijklmnopqrstuvwxyz'"/>
+                  <xsl:variable name="upper" select="'ABCDEFGHIJKLMNOPQRSTUVWXYZ'"/>
+                  <xsl:for-each select="sitemap:urlset/sitemap:url">
+                    <tr>
+                      <td>
+                        <xsl:variable name="itemURL">
+                          <xsl:value-of select="sitemap:loc"/>
+                        </xsl:variable>
+                        <a href="{$itemURL}">
+                          <xsl:value-of select="sitemap:loc"/>
+                        </a>
+                      </td>
+                      <td>
+                        <xsl:value-of select="concat(sitemap:priority*100,'%')"/>
+                      </td>
+                      <td>
+                        <xsl:value-of select="count(image:image)"/>
+                      </td>
+                      <td>
+                        <xsl:value-of select="concat(translate(substring(sitemap:changefreq, 1, 1),concat($lower, $upper),concat($upper, $lower)),substring(sitemap:changefreq, 2))"/>
+                      </td>
+                      <td>
+                        <xsl:value-of select="concat(substring(sitemap:lastmod,0,11),concat(' ', substring(sitemap:lastmod,12,5)))"/>
+                      </td>
+                    </tr>
+                  </xsl:for-each>
+                </tbody>
+              </table>
+            </div>
+          </body>
+        </html>
+      </xsl:template>
+    </xsl:stylesheet>
+    `
+
+  fs.writeFile('public/sitemap.xml', sitemap, (err) => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+
+  fs.writeFile('public/sitemap.xsl', sitemapStyle, (err) => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+}
+
+//
