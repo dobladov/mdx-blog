abccb22e31d09cb0620b179a0fd34516b68cc9e4
Daniel Doblado <danieldoblado@gmail.com>
2020-01-01 22:55:45 +0100

    refactor: Fix lint error

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import { Tag } from "react-feather"
+import { Tag } from 'react-feather'
 
 import SEO from '../components/seo'
 import Layout from '../components/layout'
