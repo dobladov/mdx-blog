bb8d5cdc24ad90944c5c42ce1ea810ea387885cd
Daniel Doblado <danieldoblado@gmail.com>
2020-01-02 22:51:52 +0100

    style: Use same color for both themes

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,6 +21,7 @@ const globalSyles = css`
     --main-bg: hsl(0, 0%, 1%);
     --main-bg-contrast: hsl(0, 0%, 6%);
     --contrast: hsl(154, 44%, 49%);
+    --contrast: hsl(154, 45%, 48%);
     --text: hsl(0, 0%, 100%);
     --important: hsl(5, 83%, 64%);
     --subtle: hsl(0, 0%, 40.8%);
@@ -52,7 +53,7 @@ const globalSyles = css`
       --main-bg: hsl(0, 0%, 96%);
       --main-bg-contrast: hsl(0, 0%, 94%);
       --text: hsl(0, 0%, 24%);
-      --contrast: hsl(5, 83%, 64%);
+      /* --contrast: hsl(5, 83%, 64%); */
       background-image: url(${backgroundLight});
     }
   }
