400585a9cc89117551cd260e32791b6c214966eb
Daniel Doblado <danieldoblado@gmail.com>
2020-01-03 15:49:21 +0100

    feat: Progress bar for aticles

--- src/components/ScrollUp.js
+++ src/components/ScrollUp.js
@@ -11,32 +11,54 @@ const style = css`
   border: 1px solid var(--main-bg-contrast);
 `
 
+const progressStyle = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 4px;
+  background-color: var(--contrast);
+`
+
 const ScrollUp = props => {
   const [showUp, setShowUp] = useState(true)
+  const [readPercentage, setReadPercentage] = useState(0)
 
   useEffect(() => {
     window.addEventListener('scroll', (e) => {
-      if (window.scrollY > 100) {
+      const currentScroll = window.scrollY
+
+      if (currentScroll > 100) {
         setShowUp(true)
       } else {
         setShowUp(false)
       }
+
+      const scrollPercent = `${currentScroll / window.scrollMaxY * 100}%`
+      setReadPercentage(scrollPercent)
     })
     setShowUp(false)
   }, [])
 
   return showUp ? (
-    <a
-      href="#"
-      title="Go Up"
-      aria-hidden="true"
-    >
-      <ChevronsUp
-        css={style}
-        width="60px"
-        height="60px"
-      />
-    </a>
+    <>
+      <a
+        href="#"
+        title="Go Up"
+        aria-hidden="true"
+      >
+        <ChevronsUp
+          css={style}
+          width="60px"
+          height="60px"
+        />
+      </a>
+
+      <div
+        css={progressStyle}
+        className="progress"
+        style={{ width: readPercentage }}
+      ></div>
+    </>
   ) : null
 }
 
