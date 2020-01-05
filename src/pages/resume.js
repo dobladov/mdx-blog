7690a16dbfe597f93dfbd026e8ffa4117c73aed6
Daniel Doblado <danieldoblado@gmail.com>
2020-01-05 23:41:49 +0100

    Add draft information for Resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,16 +3,208 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import { css } from '@emotion/core'
+import { Link } from 'gatsby'
+
+const style = css`
+  h3 {
+    display: inline-block;
+    margin-right: 10px;
+    margin-bottom: 0;
+
+    + span {
+      font-size: .9rem;
+    }
+  }
+
+  #skills {
+    ul {
+      list-style-type: none;
+      display: inline-flex;
+      flex-wrap: wrap;
+
+      li  {
+        padding: 0 10px;
+      }
+    }
+  }
+`
+
 const ProjectsPage = () => (
   <Layout>
-    <SEO title="Projects" />
-    <h1 className="title">
-      Resume
-    </h1>
-    <p>Introduction</p>
-    <h2>Experience</h2>
-    <h2>Skills</h2>
-    <h2>Education</h2>
+    <SEO title="Resume Daniel Doblado" />
+    <section css={style}>
+
+      <h1 className="title">
+        Daniel Doblado
+      </h1>
+
+      <h2>Front-End Developer</h2>
+
+      <p>I&apos;m looking for a new full-time position in Berlin or remote.</p>
+      <p>Introduction</p>
+
+      <div>
+        <h3>About</h3>
+
+        <ul>
+          <li>
+            <b>Age</b> {~~((Date.now() - Date.parse('14 Dec 1992 00:12:00 GMT')) / (31557600000))}
+          </li>
+          <li>
+            <b>Nationality</b> Spanish
+          </li>
+          <li>
+            <b>Location:</b> Berlin (Germany)
+          </li>
+          <li>
+            <b>Languages</b>
+            <ul>
+              <li>
+                English <span>B2</span>
+              </li>
+              <li>
+                Spanish <span>Native</span>
+              </li>
+              <li>
+                German <span>A2</span>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>Phone</b> <a href="tel:+49 17655574077">+49 17655574077</a>
+          </li>
+          <li>
+            <b>E-Mail</b> <a href="mailto: danieldoblado@gmail.com">danieldoblado@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="skills">
+
+        <h2 >Skills</h2>
+
+        <p>I have worked professionaly with all of this techonlogies, tools and platforms covering this <a href="https://roadmap.sh/frontend">Roadmap</a>, and I can learn any new technologies required for the job.</p>
+
+        <ul>
+          <li>ReactJS</li>
+          <li>VueJS</li>
+          <li>Svelte</li>
+          <li>Nuxt.js</li>
+          <li>Next.js</li>
+          <li>SASS</li>
+          <li>Less</li>
+          <li>Lit-html</li>
+          <li>PostCSS</li>
+          <li>Emotion-css</li>
+          <li>Tailwind </li>
+          <li>Wehooks</li>
+          <li>ES6</li>
+          <li>Node.js</li>
+          <li>Elasticsearch</li>
+          <li>Mapbox-GL</li>
+          <li>D3.js</li>
+          <li>Apache</li>
+          <li>Nginx</li>
+          <li>Python</li>
+          <li>PHP</li>
+          <li>Java</li>
+          <li>MySQL</li>
+          <li>SQLite</li>
+          <li>HTML5</li>
+          <li>Webpack</li>
+          <li>Parcel</li>
+          <li>Wordpress</li>
+          <li>Graph-QL</li>
+          <li>Git</li>
+          <li>Flask</li>
+          <li>Prestashop</li>
+          <li>Greenshok</li>
+          <li>Anime.js</li>
+          <li>Seo</li>
+          <li>Docker</li>
+          <li>Bash</li>
+          <li>DigitalOcean</li>
+          <li>Github</li>
+          <li>Gitlab</li>
+          <li>Firebase</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Experience</h2>
+
+        <h3>Graphthinking</h3> <span>2018 - Currently</span>
+
+        <ul>
+          <li>
+            Develop the <a href="https://oerworldmap.org/resource/">OER World Map</a> as the <a href="https://github.com/hbz/oerworldmap-ui/graphs/contributors">main</a> Front-End developer, this project for the <a href="https://hewlett.org/">Hewlett Foundation</a> was granted with the <a href="https://oerworldmap.wordpress.com/2018/05/03/catalysing-open-innovation-oer-world-map-at-oer18-and-oeglobal18/"> Open Inovation Award</a>, it consisted on complete refactor of the UI from the ground using React, Mapbox-GL, server side rendering, Elasticsearch, Reactive-search, and many other modern technologies and tools.
+          </li>
+          <li>
+              Develop of <a href="http://skohub.io/">SkoHub</a> in collaboration wiht <a href="https://www.hbz-nrw.de/">Hochschulbibliothekszentrum (HBZ)</a>, creating backend functionallity for automatic <a href="https://www.gatsbyjs.org/">Gatsby</a> builds using webhooks, compatible with both Github and Gitlab triggers.
+          </li>
+          <li>
+            Port the build system of <a href="https://rightsstatements.org/en/">Rights Statements</a> to a more moder system using <a href="https://www.netlify.com/">Netlify</a> improving their workflow with a more robust editorial workflow and translation integration.
+          </li>
+          <li>
+            Development of a UI for the <a href="https://www.bauhaus.de/en/">Bauhaus-Archiv</a> to access a collection of letters, photos, from Walter Gropius. Made with React, Reactive-Search and using Elasticsearch as the database.
+          </li>
+        </ul>
+
+        <h3>Dabo Consulting</h3> <span>2013-2016</span>
+
+        <ul>
+          <li>Web development of internal tools, wordpress blogs, and various other sites</li>
+          <li>Backups of all the sermers and employes data</li>
+          <li>Automatize the process for backup and scale of MySQL</li>
+          <li>IT support for the emplyees and clients</li>
+          <li>HelpDesk for clients and the comercial network</li>
+          <li>Network administration and assignation of resources</li>
+          <li>Maintenance of Linux and Windows servers</li>
+          <li>Confure of DNS, DHCP, OpenVPN, ...</li>
+          <li>Deploy and maintenance of Nginx and Apache</li>
+          <li>Automatization of processes with Bash, Cron and Python</li>
+          <li>Deploy and maintenance of VMWare virtual machines</li>
+          <li>Development and maintenance of Joomla, Wordpress, Moodle and Chamilo</li>
+        </ul>
+
+        <h3>Abire Formación</h3> <span>2013</span>
+
+        <ul>
+          <li>Desarrollo web con PHP, HTML, CSS y JS</li>
+          <li>IT support</li>
+        </ul>
+
+        <h3>Direccionate</h3> <span>2011</span>
+
+        <ul>
+          <li>Web development with Java, HTML5, CSS and Javascript.</li>
+          <li>Deplymoent and maintenance of Moodle platforms.</li>
+        </ul>
+
+        <h3>Personal Experience</h3>
+
+        <p>
+          I have been unsing Linux as my main desktop since I was 14 years old utlizing only Open-source tools for my workflow wich made me contribute constantly over the years to Open-source, this efforts can be seen on my <Link to="/projects">projects</Link> or my Github <a href="https://github.com/dobladov/">profile</a>.
+        </p>
+
+        <p>
+          I also started to use Stackoverflow in a <a href="https://stackoverflow.com/users/2498992/daniel-doblado">more intensive</a> way, finishin in the top 7% of contributors for the year and in the the top 38% of all time, in only one year.
+        </p>
+      </div>
+
+      <div>
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <b>Web Development</b> <i>NVQ Level 3</i> Doña Salvadora Muñoz
+          </li>
+          <li>
+            <b>Computing and networks</b> <i>NVQ Level 2</i> Doña Salvadora Muñoz
+          </li>
+        </ul>
+      </div>
+    </section>
   </Layout>
 )
 
