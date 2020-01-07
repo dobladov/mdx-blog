2241535faf2230d13651a69b3870f37e0f5190d6
Daniel Doblado <danieldoblado@gmail.com>
2020-01-08 00:04:43 +0100

    feat: Make the best 404 page in the world

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import { css } from '@emotion/core'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import AhShit from '../../content/assets/AhShit.png'
+
+const style = css`
+  max-width: 1000px;
+  margin: 0 auto;
+
+  img {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    max-height: 90vh;
+  }
+
+  h2 {
+    font-size: 3rem;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+`
+
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      css={style}
+    >
+      <h1 className="title">NOT FOUND</h1>
+      <h2>You picked the wrong <b>URL</b>, fool!</h2>
+      <p>AY, AY AY AY! Go back to <Link to="/">home</Link>, chill, chill!</p>
+      <img
+        src={AhShit}
+        alt="Ah shit here we go again."
+      />
+    </div>
   </Layout>
 )
 
